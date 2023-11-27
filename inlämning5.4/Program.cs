f50fd0aef61da619eb52994c6db74e59fa5a1022
Elm2006 <isel06002@utb.vaxjo.se>
2023-11-27 11:30:53 +0100

    Add project files.

---
+++ inlämning5.4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace inlämning5.__4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = new string[] { "Isak", "Thoe", "Theo", "Sj", "Mako" };
+            for(int i = 0; i < namn.Length; i++)
+            {
+                Array.Sort(namn);
+                Console.WriteLine(namn[i]);
+            }
+        }
+    }
+}
